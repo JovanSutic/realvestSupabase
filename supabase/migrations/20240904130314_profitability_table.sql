d095fb05a49ecf37fb329305414092c6698396f5
unknown <josutic.sutic@gmail.com>
2024-09-05 19:06:39 +0200

    feat(86c074nm2): add changes to the profitability table

--- supabase/migrations/20240904130314_profitability_table.sql
+++ supabase/migrations/20240904130314_profitability_table.sql
@@ -1,37 +1,41 @@
 CREATE TABLE IF NOT EXISTS "public"."ad_profitability"(
     "id" serial primary key NOT NULL,
     "averageCompetition" float,
+    "medianCompetition" float,
     "minCompetition" float,
     "maxCompetition" float,
+    "competitionNewBuildAverage" float,
     "competitionCount" integer,
-    "unsoldCompetitionCount" integer,
+    "competitionNewBuildCount" integer,
     "cityCountSold" integer,
     "cityCountAds" integer,
+    "cityAverage" float,
     "competitionTrend" float,
     "averageRental" float,
     "minRental" float,
     "maxRental" float,
     "rentalCount" integer,
-    "activeRentalCount" integer
+    "ad_type" varchar(50),
+    "ad_id" bigint
 );
 
-ALTER TABLE apartments
-ADD ad_profitability_id bigint,
-ADD CONSTRAINT ad_profitability_id 
-FOREIGN KEY (ad_profitability_id) 
-REFERENCES ad_profitability (id)
-ON DELETE SET NULL;
+-- ALTER TABLE apartments
+-- ADD ad_profitability_id bigint,
+-- ADD CONSTRAINT ad_profitability_id 
+-- FOREIGN KEY (ad_profitability_id) 
+-- REFERENCES ad_profitability (id)
+-- ON DELETE SET NULL;
 
-ALTER TABLE commercials
-ADD ad_profitability_id bigint,
-ADD CONSTRAINT ad_profitability_id 
-FOREIGN KEY (ad_profitability_id) 
-REFERENCES ad_profitability (id)
-ON DELETE SET NULL;
+-- ALTER TABLE commercials
+-- ADD ad_profitability_id bigint,
+-- ADD CONSTRAINT ad_profitability_id 
+-- FOREIGN KEY (ad_profitability_id) 
+-- REFERENCES ad_profitability (id)
+-- ON DELETE SET NULL;
 
-ALTER TABLE garages
-ADD ad_profitability_id bigint,
-ADD CONSTRAINT ad_profitability_id 
-FOREIGN KEY (ad_profitability_id) 
-REFERENCES ad_profitability (id)
-ON DELETE SET NULL;
+-- ALTER TABLE garages
+-- ADD ad_profitability_id bigint,
+-- ADD CONSTRAINT ad_profitability_id 
+-- FOREIGN KEY (ad_profitability_id) 
+-- REFERENCES ad_profitability (id)
+-- ON DELETE SET NULL;
